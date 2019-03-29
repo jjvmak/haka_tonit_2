f882bd541ff934dc86a559073797ad1166fbf9aa
mialky <mialky@outlook.com>
2019-03-29 21:02:47 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,16 +1,16 @@
 // secret key
 const key = "haka_ton2";
 
-// Node requirements
+// reqs
 const express = require("express");
 const app = express();
 app.use(express.json());
 
-// Handle GET root
+// handlers
 app.get("/", (req, res) => {
   console.log("somebody meddled with root");
   res.send("GET OUT OF MY ROOT!");
 });
 
-// Open to localhost:6900
+// open to localhost:6900
 app.listen(6900, () => console.log("Ready to gamibinify in port 6900.."));
