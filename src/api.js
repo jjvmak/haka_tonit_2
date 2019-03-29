8d5a54698c32a9afd3650ace232e7ee7de9997f1
mialky <mialky@outlook.com>
2019-03-29 21:24:59 +0200

    cors

--- src/api.js
+++ src/api.js
@@ -6,6 +6,28 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+// cors
+const modelFullId = client.modelPath(projectId, computeRegion, modelId);
+const cors = require("cors");
+const bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 500000000000000000000000000
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // handlers
 app.get("/", (req, res) => {
   console.log("somebody meddled with root");
