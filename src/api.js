29f24d2c3af996c03b893f572e8a8f9fe7631f13
mialky <mialky@outlook.com>
2019-03-29 22:29:53 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -29,16 +29,33 @@ app.use((req, res, next) => {
 });
 
 // datas
+const customers = fs.readFileSync("../rData/customers/customers.json");
+const employees = fs.readFileSync("../rData/employees/employees.json");
+// products
 const products = fs.readFileSync("../rData/products/products.json");
+// stores
+const stores = fs.readFileSync("../rData/stores/stores.json");
 
 // handlers
 app.get("/", (req, res) => {
   console.log("somebody meddled with root");
   res.send("GET OUT OF MY ROOT!");
 });
+app.get("/customers", (req, res) => {
+  console.log(":D");
+  res.send(JSON.parse(customers));
+});
+app.get("/employees", (req, res) => {
+  console.log(":D");
+  res.send(JSON.parse(employees));
+});
 app.get("/products", (req, res) => {
   console.log(":D");
   res.send(JSON.parse(products));
+});
+app.get("/stores", (req, res) => {
+  console.log(":D");
+  res.send(JSON.parse(stores));
 });
 
 // open to localhost:6900
