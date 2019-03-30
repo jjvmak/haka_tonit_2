93ad040b18a2799637be9bfac6ea412fa20ddd28
mialky <mialky@outlook.com>
2019-03-30 02:36:27 +0200

    mapplosion :de:

--- bucketfinder/src/App.js
+++ bucketfinder/src/App.js
@@ -1,28 +1,69 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import GoogleMapReact from "google-map-react";
+import "./App.css";
 
-class App extends Component {
+// Google Maps
+var APIkey = "AIzaSyChKzCo94RAilpNe8MQOzBMjYC7XhuaEGs";
+
+// map styling
+const mapStyle = {
+  height: "50vh"
+};
+const markerStyle = {
+  height: "50px",
+  width: "50px",
+  marginTop: "-50px"
+};
+const imgStyle = {
+  height: "100%"
+};
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <div
+          style={{
+            // header
+            height: "15vh",
+            width: "100%",
+            backgroundColor: "grey"
+          }}
+        />
+        <div style={{ height: "70vh", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: APIkey }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
           >
-            Learn React
-          </a>
-        </header>
+            <AnyReactComponent
+              lat={59.955413}
+              lng={30.337844}
+              text="My Marker"
+            />
+          </GoogleMapReact>
+        </div>
+        <div
+          style={{
+            // footer
+            height: "15vh",
+            width: "100%",
+            backgroundColor: "grey"
+          }}
+        />
       </div>
     );
   }
 }
 
-export default App;
+export default SimpleMap;
