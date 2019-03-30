f303d6cc0ea681ead947ef6bc92682bc15c03d51
Joonas Mäkinen <jjvmak@utu.fi>
2019-03-30 09:07:40 +0200

    Refactored the filtering of products. Some reformatting.

--- src/api.js
+++ src/api.js
@@ -13,105 +13,92 @@ const _ = require("underscore");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 app.use(cors());
-app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.json({limit: "50mb"}));
 app.use(
-  bodyParser.urlencoded({
-    limit: "50mb",
-    extended: true,
-    parameterLimit: 500000000000000000000000000
-  })
+    bodyParser.urlencoded({
+        limit: "50mb",
+        extended: true,
+        parameterLimit: 500000000000000000000000000
+    })
 );
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
 });
 
 // datas
 const customers = JSON.parse(
-  fs.readFileSync("../rData/customers/customers.json")
+    fs.readFileSync("../rData/customers/customers.json")
 );
 const employees = JSON.parse(
-  fs.readFileSync("../rData/employees/employees.json")
+    fs.readFileSync("../rData/employees/employees.json")
 );
 const products = JSON.parse(fs.readFileSync("../rData/products/products.json"));
 const stores = JSON.parse(fs.readFileSync("../rData/stores/stores.json"));
 
 // product-types
+/** Use word list as a filter. Word lists can be modified later on, to add more products to each categories.
+ * Note that filtering buckets works bit different. */
 var foodWords = ["Pizza", "Chocolate Bar", "Gum", "Bread"];
 const foods = products.filter(
-  product =>
-    product.productType === "Pizza" ||
-    product.productType === "Chocolate Bar" ||
-    product.productType === "Gum" ||
-    product.productType === "Bread"
+    product => foodWords.includes(product.productType)
 );
+var drinkWords = ["Premium Cider", "Alcohol Free Wine", "Special Water", "Cider", "Lemonade", "Cola"];
 const drinks = products.filter(
-  product =>
-    product.productType === "Premium Cider" ||
-    product.productType === "Alcohol Free Wine" ||
-    product.productType === "Beer" ||
-    product.productType === "Special Water" ||
-    product.productType === "Cider" ||
-    product.productType === "Lemonade" ||
-    product.productType === "Cola"
+    product => drinkWords.includes(product.productType)
 );
+var containerWords = ["Bucket", "Basket"];
 const containers = products.filter(
-  product =>
-    product.productType === "Bucket" || product.productType === "Basket"
+    product => containerWords.includes(product.productType)
 );
 const buckets = products.filter(product => product.productType === "Bucket");
+var miscWords = ["Notebook", "Helmet", "Keychain", "Tin Foil", "Pencil", "Helmet"];
 const misc = products.filter(
-  product =>
-    product.productType === "Notebook" ||
-    product.productType === "Helmet" ||
-    product.productType === "Keychain" ||
-    product.productType === "Tin Foil" ||
-    product.productType === "Pencil" ||
-    product.productType === "Helmet"
+    product => miscWords.includes(product.productType)
 );
 
 // handlers
 app.get("/api/customers", (req, res) => {
-  console.log(":D");
-  res.send(customers);
+    console.log(":D");
+    res.send(customers);
 });
 app.get("/api/employees", (req, res) => {
-  console.log(":D");
-  res.send(employees);
+    console.log(":D");
+    res.send(employees);
 });
 app.get("/api/products", (req, res) => {
-  console.log(":D");
-  res.send(products);
+    console.log(":D");
+    res.send(products);
 });
 app.get("/api/stores", (req, res) => {
-  console.log(":D");
-  res.send(stores);
+    console.log(":D");
+    res.send(stores);
 });
 
 // product-types
 app.get("/api/products/foods", (req, res) => {
-  console.log(":D");
-  res.send(foods);
+    console.log(":D");
+    res.send(foods);
 });
 app.get("/api/products/drinks", (req, res) => {
-  console.log(":D");
-  res.send(drinks);
+    console.log(":D");
+    res.send(drinks);
 });
 app.get("/api/products/containers", (req, res) => {
-  console.log(":D");
-  res.send(containers);
+    console.log(":D");
+    res.send(containers);
 });
 app.get("/api/products/containers/buckets", (req, res) => {
-  console.log(":D");
-  res.send(buckets);
+    console.log(":D");
+    res.send(buckets);
 });
 app.get("/api/products/misc", (req, res) => {
-  console.log(":D");
-  res.send(misc);
+    console.log(":D");
+    res.send(misc);
 });
 
 // open to localhost:6900
